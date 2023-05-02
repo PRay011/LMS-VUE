848f600aabff84840bd6ab59426665caf1f7220e
PRay <2519219483@qq.com>
2023-05-02 14:56:20 +0800

    登陆界面

--- src/pages/User/login.vue
+++ src/pages/User/login.vue
@@ -1,18 +1,37 @@
 <template>
-    
+    <navigationBar></navigationBar>
+    <div class="container">
+        <div class="box">
+           <div class="left">
+           </div>
+           <div class="right">
+                <div class="title">欢迎登录</div>
+                <el-input v-model="accout" placeholder="请输入账号" style="" />
+                <el-input v-model="password" placeholder="请输入密码" />
+                <div style="margin-top: 10px;"></div>
+                <el-text class="forget" type="primary">忘记密码？</el-text>
+                <div style="margin-top: 90px;"></div>
+                <el-button class="login">登录</el-button>
+                <el-button class="register">注册</el-button>
+           </div>
+        </div>
+    </div>
 </template>
   
 <script>
   import { defineComponent } from "vue"
   import '../../utils/login.js'
-  
+  import navigationBar from '../../components/header.vue'
+
   export default defineComponent({
       name: "login",
       data(){
           return {
-              data:''
+              accout:'',
+              password:'',
           }
       },
+      components: {navigationBar},
       methods: {
           
       },
@@ -23,4 +42,53 @@
 <style scoped>
 @import '../../assets/css/Login.css';
 
+::v-deep .el-input{
+    width:60%;
+    height:7%;
+    margin-top:2%;
+    margin-left: 10%;
+    font-size:17px;
+
+}
+.el-input >>> .el-input__inner {
+    width:100px;
+} 
+
+.forget{
+    margin-left: 10%;
+}
+
+::v-deep .login{
+    margin-left: 10%;
+    height: 7%;
+    width: 20%;
+    font-size:15px;
+    color:white;
+    border:1px solid white;
+    background-color:rgb(2,70,143);
+}
+
+.login:hover{
+    color:rgb(2,70,143);
+    background-color:white;
+    border:1px solid rgb(2,70,143);
+    transition-duration: 0.5s;
+}
+
+::v-deep .register{
+    margin-left: 7%;
+    height: 7%;
+    width: 20%;
+    font-size:15px;
+    color:rgb(2,70,143);
+    border:1px solid rgb(2,70,143);
+    background-color: white;
+}
+
+.register:hover{
+    color:white;
+    background-color:rgb(2,70,143);
+    border:1px solid white;
+    transition-duration: 0.5s;
+}
 </style>
